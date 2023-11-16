99a2fda4e47582f99c2a90b1c68b49c83dd91d28
Pavel Kornev <pavel.kornev@sap.com>
2023-11-16 12:23:48 +0100

    Spectral runtime failure

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,15 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { stylish, FormatterOptions } from "@stoplight/spectral-formatters";
 import * as Parsers from '@stoplight/spectral-parsers';
-import { defined } from "@stoplight/spectral-functions";
-import { DiagnosticSeverity } from "@stoplight/types";
+// import { DiagnosticSeverity } from "@stoplight/types";
+import { oas } from "@stoplight/spectral-rulesets";
 
 const source = path.join(__dirname, "./oas-example.json");
 const document = new Document(fs.readFileSync(source, "utf8"), Parsers.Json, source);
 
 const ruleset = new Ruleset({
-  rules: {
-    "error-code-defined": {
-      message: "`code` property is missing in the Error object definition",
-      severity: DiagnosticSeverity.Error,
-      given: "$.paths.*.*.responses[?(@property.match(/^(4|5)/))].content.'application/json'.schema.properties.error.properties",
-      then: {
-        field: "code",
-        function: defined,
-      },
-    }
-  },
+  extends: [oas],
+  rules: {},
 });
 
 const spectral = new Spectral();
