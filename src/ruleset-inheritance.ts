350ed0f1798f58044354e9267517ed3a4ed11134
Pavel Kornev <pavel.kornev@sap.com>
2026-02-06 17:17:02 +0100

    Add rule inheritance issue

---
+++ src/ruleset-inheritance.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import { Document, Spectral, Ruleset, RulesetDefinition } from "@stoplight/spectral-core";
+import * as Parsers from '@stoplight/spectral-parsers';
+import { oas } from "@stoplight/spectral-rulesets";
+import { oas3 } from "@stoplight/spectral-formats";
+import { printMemoryUsage } from './monitor_memory.js';
+import logUpdate from 'log-update';
+import { defined } from '@stoplight/spectral-functions';
+
+const ruleset1: RulesetDefinition = {
+  extends: [[oas, "off"]],
+  rules: {
+    "operation-success-response": true,
+    "foo-bar": {
+      formats: [oas3],
+      given: "$",
+      then: {
+        function: defined
+      }
+    }
+  },
+};
+
+const ruleset2 = {
+//   extends: [ruleset1],
+  extends: [[ruleset1, "off"]],
+};
+
+const ruleset3 = {
+//   extends: [ruleset2],
+  extends: [[ruleset2, "off"]],
+};
+
+
+
+const rulesetInstance = new Ruleset(ruleset2);
+
+console.log("Enabled rules: ", Object.keys(rulesetInstance.rules).filter(ruleName => rulesetInstance.rules[ruleName].enabled).join(", "));
